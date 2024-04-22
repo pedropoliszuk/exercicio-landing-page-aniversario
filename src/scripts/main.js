9d3a761dc7f730f72c716c5db30dbe3c150c3895
Pedro Poliszuk <pedropoliszuk2@live.com>
2024-04-21 23:22:37 -0300

    finalizando o exercicio

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+const dataDoEvento = new Date("Apr 04, 2025 19:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0 ) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado :(`;
+    }
+}, 1000);
